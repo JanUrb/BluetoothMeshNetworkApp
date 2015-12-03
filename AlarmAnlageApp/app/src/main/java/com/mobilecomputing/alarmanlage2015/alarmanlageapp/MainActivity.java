659d6bedb0f9f07b088312f9cb0a400ff5662f3b
KratBro <urbans.ky@web.de>
2015-12-03 13:48:08 +0100

    Setup für die Alarmanlagen-App

---
+++ AlarmAnlageApp/app/src/main/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mobilecomputing.alarmanlage2015.alarmanlageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
