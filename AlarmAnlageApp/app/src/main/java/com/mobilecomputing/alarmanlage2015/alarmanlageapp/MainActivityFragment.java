52a7c711e2454f75475f973082fde6efc2b9ad82
KratBro <KratBro@users.noreply.github.com>
2015-12-19 21:54:12 +0100

    GUI-Elemente dem Fragment zur Verfügung gestellt.

--- AlarmAnlageApp/app/src/main/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/MainActivityFragment.java
+++ AlarmAnlageApp/app/src/main/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import fllog.Log;
 
@@ -14,7 +19,26 @@ import fllog.Log;
 public class MainActivityFragment extends Fragment {
     private static final String TAG ="fhflMainActFragment";
 
+    /**
+     * Sendet eine Nachricht zur im address_input angegebenen BT_ADDR
+     */
+    private Button sendButton;
+    /**
+     * Enthält eine BT_ADDR
+     */
+    private EditText address_input;
+    /**
+     * Listet die direkt verbundenen Geräte auf (BT_ADDR)
+     */
+    private TextView device_list;
 
+    /**
+     * Stellt die eigenen BT_ADDR dar.
+     */
+    private TextView my_bt_addr;
+    /**
+     *  Konstruktor
+     */
     public MainActivityFragment() {
         Log.d(TAG, "MainActivityFragment()");
     }
@@ -24,6 +48,10 @@ public class MainActivityFragment extends Fragment {
         Log.d(TAG, "onCreateView()");
 
         View view = inflater.inflate(R.layout.activity_main_fragment, container, false);
+        sendButton = (Button) view.findViewById(R.id.send_message_button);
+        address_input = (EditText) view.findViewById(R.id.send_to_input);
+        device_list = (TextView) view.findViewById(R.id.connected_devices_list);
+        my_bt_addr = (TextView) view.findViewById(R.id.my_bt_addr);
 
 
         /*Benötigte Elemente: Liste der verbundenen Geräte, Eingabefeld zum Senden an eine Adresse, ein senden Knopf*/
