94ee0aecb8a4ce396a65ff23811a7bbc4fca6863
KratBro <KratBro@users.noreply.github.com>
2015-12-29 19:01:43 +0100

    prep

--- AlarmAnlageApp/app/src/main/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/ConnectThread.java
+++ AlarmAnlageApp/app/src/main/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/ConnectThread.java
@@ -37,7 +37,7 @@ public class ConnectThread extends Thread {
         // Get a BluetoothSocket to connect with the given BluetoothDevice
         try {
             // MY_UUID is the app's UUID string, also used by the server code
-            tmp = device.createRfcommSocketToServiceRecord(my_uuid);
+            tmp = mmDevice.createRfcommSocketToServiceRecord(my_uuid);
         } catch (IOException e) { }
         mmSocket = tmp;
     }
