d8da8a89b3e34d2763cd5735866b07ea814c99ce
KratBro <KratBro@users.noreply.github.com>
2016-01-04 18:49:44 +0100

    Message Klasse

---
+++ AlarmAnlageApp/app/src/main/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/Message.java
@@ -0,0 +1,54 @@
+package com.mobilecomputing.alarmanlage2015.alarmanlageapp;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * Die Message-Klasse besteht aus der MAC-Adresse des Empfängers und aus einer ID.
+ * Die ID wird beim Aufrufen des Konstruktors erstellt.
+ *
+ * Serializeable erlaubt das Umformen in ein byte[]
+ *
+ * <p/>
+ * Created by Jan Urbansky on 04.01.2016.
+ */
+public class Message implements Serializable{
+    private UUID messageId;
+    private String messageTargetMac;
+    private String messageSourceMac;
+
+
+    public Message(String messageSourceMac, String messageTargetMac) {
+        this.messageSourceMac = messageSourceMac;
+        this.messageTargetMac = messageTargetMac;
+        messageId = UUID.randomUUID();
+    }
+
+    public UUID getMessageId() {
+        return messageId;
+    }
+
+    public String getMessageSourceMac() {
+        return messageSourceMac;
+    }
+
+    public String getMessageTargetMac() {
+        return messageTargetMac;
+    }
+
+    /**
+     * Quelle: https://stackoverflow.com/questions/5837698/converting-any-object-to-a-byte-array-in-java
+     * @return
+     * @throws IOException
+     */
+    public byte[] getBytes() throws IOException {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        ObjectOutputStream o = null;
+        o = new ObjectOutputStream(b);
+        o.writeObject(this);
+        return b.toByteArray();
+    }
+}
