7fd48619287edee70ceccc30509008bd88490c7d
KratBro <KratBro@users.noreply.github.com>
2016-01-07 12:49:39 +0100

    Wird nicht gebraucht!

--- AlarmAnlageApp/app/src/main/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/communication/CommunicationInterface.java
+++
@@ -1,9 +0,0 @@
-package com.mobilecomputing.alarmanlage2015.alarmanlageapp.communication;
-
-/**
- * Created by Donskelle-PC on 15.12.2015.
- *
- */
-public interface CommunicationInterface {
-    public boolean sendMessage(String Message);
-}
