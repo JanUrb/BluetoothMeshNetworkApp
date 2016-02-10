52e2afd6af3d3264fee9381d3c0129f70dc389c4
KratBro <KratBro@users.noreply.github.com>
2016-02-10 22:50:32 +0100

    Setting up Tests

---
+++ AlarmAnlageApp/app/src/test/java/com/mobilecomputing/alarmanlage2015/alarmanlageapp/BluetoothModelTest.java
@@ -0,0 +1,61 @@
+package com.mobilecomputing.alarmanlage2015.alarmanlageapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jan Urbansky on 10.02.2016.
+ */
+public class BluetoothModelTest {
+
+    @Test
+    public void testSetMyBT_ADDR() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCurrentMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMyBT_ADDR() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNumberOfConnections() throws Exception {
+
+    }
+
+    @Test
+    public void testAddConnection() throws Exception {
+
+    }
+
+    @Test
+    public void testIsDeviceAlreadyConnected() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConnections() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveConnection() throws Exception {
+
+    }
+
+    @Test
+    public void testNotifyObservers() throws Exception {
+
+    }
+}
