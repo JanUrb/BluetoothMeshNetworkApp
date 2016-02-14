9103c920d3f07fb713b13fcb8c9c770e125dfd49
KratBro <KratBro@users.noreply.github.com>
2016-02-14 22:24:01 +0100

    Final Interface Design

--- AlarmAnlageApp/app/src/main/java/BluetoothCommunication/ICommunicationEvents.java
+++ AlarmAnlageApp/app/src/main/java/BluetoothCommunication/ICommunicationEvents.java
@@ -8,20 +8,20 @@ public interface ICommunicationEvents {
     /**
      * Triggers only on messages directed to the client.
      */
-    public void onDirectMessageReceived();
+    public void onDirectMessageReceived(Message message);
 
     /**
      * Triggers on every message received. Directed to the client or not.
      */
-    public void onMessageReceived();
+    public void onMessageReceived(Message message);
 
     /**
      * Triggers if a connection with a device is established.
      */
-    public void onConnectedEstablished();
+    public void onConnectedEstablished(BluetoothConnection runningConnection);
 
     /**
      * Triggers if the connection to a device is disconnected.
      */
-    public void onConnectionDisconnected();
+    public void onConnectionDisconnected(BluetoothConnection disconnectedConnection);
 }
